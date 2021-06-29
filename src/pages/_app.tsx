41beef43b5fd4018266035e3ae7d8a7044dda030
renanlimabl <cursosprogramacoes@gmail.com>
2021-06-29 06:16:31 -0300

    add styled-components and globalStyle

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado</title>
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <link rel="apple-touch-icon" href="/img/favicon.ico" />
+        <meta
+          name="description"
+          content="Um simples projeto started to work with Typescript"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
